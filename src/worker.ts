ae47f1bfcc678f9ea31fd07c60d7a75731a1a217
scwlkr <206951249+scwlkr@users.noreply.github.com>
2026-08-19 13:14:58 -0500

    Force fresh root asset through Worker

--- src/worker.ts
+++ src/worker.ts
@@ -4,6 +4,12 @@ interface Env {
 
 export default {
   fetch(request: Request, env: Env): Promise<Response> {
+    const url = new URL(request.url);
+    if (url.pathname === "/") {
+      url.pathname = "/index.html";
+      return env.ASSETS.fetch(new Request(url, request));
+    }
+
     return env.ASSETS.fetch(request);
   },
 } satisfies ExportedHandler<Env>;
